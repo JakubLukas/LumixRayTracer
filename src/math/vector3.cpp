b3f4ccbba095f0307b1c4b2d5a24efe42f681f59
jakub_lukas <lukasik.lukasik@gmail.com>
2015-10-23 09:22:39 +0200

    vector optimization of functions arguments

--- src/math/vector3.cpp
+++ src/math/vector3.cpp
@@ -9,17 +9,17 @@ Vector3::Vector3()
 {
 }
 
-Vector3::Vector3(const float &a, const float &b, const float &c)
+Vector3::Vector3(const float a, const float b, const float c)
 	: Vec3(a, b, c)
 {
 }
 
-Vector3::Vector3(const Lumix::Vec3 &v)
+Vector3::Vector3(const Lumix::Vec3 v)
 	: Vec3(v)
 {
 }
 
-Vector3& Vector3::operator *= (const float &f)
+Vector3& Vector3::operator *= (const float f)
 {
 	x *= f;
 	y *= f;
@@ -28,22 +28,22 @@ Vector3& Vector3::operator *= (const float &f)
 	return *this;
 }
 
-Vector3 operator *(const float &f, Vector3 v)
+Vector3 operator *(const float f, Vector3 v)
 {
 	return v *= f;
 }
 
-Vector3 operator *(Vector3 v, const float &f)
+Vector3 operator *(Vector3 v, const float f)
 {
 	return v *= f;
 }
 
-float Vector3::Dot(const Vector3 &v1, const Vector3 &v2)
+float Vector3::Dot(const Vector3 v1, const Vector3 v2)
 {
 	return Lumix::dotProduct(v1, v2);
 }
 
-Vector3 Vector3::Cross(const Vector3 &v1, const Vector3 &v2)
+Vector3 Vector3::Cross(const Vector3 v1, const Vector3 v2)
 {
 	return Lumix::crossProduct(v1, v2);
 }
--- src/math/vector3.h
+++ src/math/vector3.h
@@ -9,16 +9,16 @@ namespace LumixRayTracer
 struct Vector3 : public Lumix::Vec3
 {
 	Vector3();
-	Vector3(const float &a, const float &b, const float &c);
-	Vector3(const Lumix::Vec3 &v);
+	Vector3(const float a, const float b, const float c);
+	Vector3(const Lumix::Vec3 v);
 
-	Vector3& operator *=(const float &f);
+	Vector3& operator *=(const float f);
 
-	static float Dot(const Vector3 &v1, const Vector3 &v2);
-	static Vector3 Cross(const Vector3 &v1, const Vector3 &v2);
+	static float Dot(const Vector3 v1, const Vector3 v2);
+	static Vector3 Cross(const Vector3 v1, const Vector3 v2);
 };
 
-Vector3 operator *(const float &f, Vector3 v);
-Vector3 operator *(Vector3 v, const float &f);
+Vector3 operator *(const float f, Vector3 v);
+Vector3 operator *(Vector3 v, const float f);
 
 } // ~ namespace LumixRayTracer
